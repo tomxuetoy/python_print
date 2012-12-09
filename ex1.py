fb30a7a0ddf49de4e796739d2eab94fef11bc526
Tom Xue <tomxue0126@gmail.com>
2012-12-09 18:54:44 +0800

    add new print item

--- ex1.py
+++ ex1.py
@@ -4,4 +4,5 @@ print "I like typing this."
 print "This is fun."
 print 'Yay! Printing.'
 print "I'd much rather you 'not'."
+# print "I'd much "rather" you 'not'." // error
 print 'I "said" do not touch this.'
